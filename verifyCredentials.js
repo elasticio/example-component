816f313a30302aa83c58a802460a9eb25cb0dc4a
Benn McGregor <benn.mcgregor@elastic.io>
2020-04-16 00:51:41 +0200

    more changes

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -3,7 +3,7 @@ const ExampleClient = require('./lib/exampleClient');
 module.exports = async function verify(credentials, callback) {
   try {
     const client = new ExampleClient(this, credentials);
-    const result = await client.makeRequest({
+    await client.makeRequest({
       url: 'posts/1',
       method: 'GET',
     });
