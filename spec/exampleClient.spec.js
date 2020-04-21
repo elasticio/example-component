1552f861d3c376ada3e44b48cb60a39f49b56e4e
Benn McGregor <benn.mcgregor@elastic.io>
2020-04-21 21:49:56 +0200

    temporarily removed pretest

---
+++ spec/exampleClient.spec.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const bunyan = require('bunyan');
+const ExampleClient = require('../lib/exampleClient');
+
+const cfg = {};
+
+const emitter = { logger: bunyan.createLogger({ name: 'dummy' }) };
+
+describe('Example Client', () => {
+
+});
