db9f770f8a2ea96c20f07aa47edb6b6bc55ef2da
EmptyInfinity <soroka14021998@gmail.com>
2022-06-23 14:32:21 +0300

    update

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,13 @@
-/* eslint-disable class-methods-use-this */
+/* eslint-disable no-restricted-syntax, class-methods-use-this */
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const RETRIES_COUNT = 3;
+const RETRY_DELAY = 10000;
+const REQUEST_TIMEOUT = 15000;
+
+export const sleep = async (ms: number) => new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
 
 export default class Client {
   private logger: any;
@@ -12,6 +20,24 @@ export default class Client {
   }
 
   async apiRequest(opts: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.request(opts);
+    let response;
+    let currentRetry = 0;
+    while (currentRetry < RETRIES_COUNT) {
+      try {
+        response = await axios.request({
+          ...opts,
+          timeout: REQUEST_TIMEOUT,
+          validateStatus: (status) => status < 500
+        });
+      } catch (err) {
+        this.logger.debug('Error to JSON', err.toJSON());
+        this.logger.debug('Error message', err.message);
+        this.logger.error(`Got error "${err.response?.statusText}", status - ${err.response?.status}, body: ${JSON.stringify(err.response?.data)}`);
+        this.logger.info(`Request failed, retrying (${1 + currentRetry})`);
+        await sleep(RETRY_DELAY);
+        currentRetry++;
+      }
+    }
+    return response;
   }
 }
