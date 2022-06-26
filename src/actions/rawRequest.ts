4e0e0817d8c13b6359b87978a659a132e83c131a
EmptyInfinity <soroka14021998@gmail.com>
2022-06-26 21:35:28 +0300

    update

--- src/actions/rawRequest.ts
+++ src/actions/rawRequest.ts
@@ -1,10 +1,16 @@
 import { messages } from 'elasticio-node';
 import Client from '../client';
+import inSchema from '../schemas/actions/rawRequest.in.json';
+
+const allowedMethods = inSchema.properties.method.enum;
 
 export async function processAction(msg, cfg) {
   this.logger.info('"Make Raw Request" action started');
   const client = new Client(this, cfg);
   const { url, method, data } = msg.body;
+  if (!allowedMethods.includes(method)) {
+    throw new Error(`Method "${method}" is not supported! Supported methods are: ${JSON.stringify(allowedMethods)}`);
+  }
   const response = await client.apiRequest({ url, method, data });
   this.logger.info('request is done, emitting...');
   return messages.newMessageWithBody({
