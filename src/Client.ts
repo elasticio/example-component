e987a8e5a3ea4f71f44950f0797c9a20282858fb
if0s <if0s.3d@gmail.com>
2024-01-19 16:33:29 +0200

    up

--- src/Client.ts
+++ src/Client.ts
@@ -1,20 +1,54 @@
 /* eslint-disable no-restricted-syntax, class-methods-use-this */
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { axiosReqWithRetryOnServerError } from '@elastic.io/component-commons-library';
+import * as commons from '@elastic.io/component-commons-library';
+
+const API_BASE_URL = 'https://example.com/v1/';
+const MAXIMUM_4XX_ERRORS_RETRIES = 5;
 
 export default class Client {
   private logger: any;
 
   private cfg: any;
 
+  private retries: number;
+
   constructor(context, cfg) {
     this.logger = context.logger;
     this.cfg = cfg;
+    this.retries = MAXIMUM_4XX_ERRORS_RETRIES;
   }
 
   setLogger(logger) { this.logger = logger; }
 
   async apiRequest(opts: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axiosReqWithRetryOnServerError.call(this, opts);
+    opts = {
+      ...opts,
+      baseURL: API_BASE_URL,
+      headers: {
+        ...opts.headers || {},
+        Authorization: `Custom ${this.cfg.username}:${this.cfg.password}`
+      }
+    };
+    let response;
+    let error;
+    let currentRetry = 0;
+
+    while (currentRetry <= this.retries) {
+      try {
+        response = await commons.axiosReqWithRetryOnServerError.call(this, opts);
+        return response;
+      } catch (err) {
+        error = err;
+        if (err.response?.status === 429) {
+          const retryAfter = err.response.headers['retry-after'] || 2 ** (currentRetry + 1);
+          this.logger.error(`Going to retry after ${retryAfter}sec (${currentRetry + 1} of ${this.retries})`);
+          await commons.sleep(retryAfter * 1000);
+        } else {
+          throw err;
+        }
+      }
+      currentRetry++;
+    }
+    throw error;
   }
 }
