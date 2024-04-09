95568322039c5314e1440c7f27f4d1655b2cfe7d
if0s <if0s.3d@gmail.com>
2024-04-09 16:24:03 +0300

    init

--- src/actions/rawRequest.ts
+++ src/actions/rawRequest.ts
@@ -1,37 +1,18 @@
 import { messages } from 'elasticio-node';
 
 export async function processAction(msg, cfg, snapshot) {
-  this.logger.info(`snapshot: ${JSON.stringify(snapshot || {})}`);
-  const newSnapshot = { currentTime: new Date() };
-  await this.emit('snapshot', newSnapshot);
-  this.logger.info('newSnapshot is done');
-  return messages.newMessageWithBody({});
+  return messages.newMessageWithBody({ selected: cfg.dynamicFields });
 }
 
 export const getMetaModel = async function getMetaModel(cfg) {
-  const inMeta = {
-    type: 'object',
-    properties: {
-      objectWithProperties: {
-        type: 'object',
-        help: { description: 'Object description' },
-        properties: {
-          'Some Obj key': {
-            type: 'string',
-            help: { description: 'Key description' }
-          }
-        }
-      },
-      objectWithOutProperties: {
-        type: 'object',
-        help: { description: 'Object description' },
-        properties: { }
-      }
-    }
-  };
+  return { in: {}, out: {}, };
+};
 
-  return { in: inMeta, out: {}, };
+export const getDynamicFields = async function getDynamicFields(cfg) {
+  const { cfgForDynamicFields } = cfg;
+  return JSON.parse(cfgForDynamicFields);
 };
 
 module.exports.process = processAction;
 module.exports.getMetaModel = getMetaModel;
+module.exports.getDynamicFields = getDynamicFields;
