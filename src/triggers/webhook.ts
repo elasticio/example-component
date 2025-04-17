6722f82aac4d4b3acd4a0f70f6efc13fcf5c55a1
if0s <if0s.3d@gmail.com>
2025-04-17 11:12:28 +0300

    test

---
+++ src/triggers/webhook.ts
@@ -0,0 +1,20 @@
+import { messages } from 'elasticio-node';
+
+export async function processTrigger(msg: any, cfg: any) {
+  this.logger.info('"Webhook" trigger started');
+
+  if (!msg.body) {
+    this.logger.warn('No data found, execution will be skipped');
+    return;
+  }
+
+  const reply = messages.newMessageWithBody(msg.body || {});
+  reply.headers['X-EIO-Routing-Key'] = msg.headers.reply_to;
+  reply.headers['Content-Type'] = 'application/json';
+  reply.headers['x-eio-status-code'] = 200;
+  await this.emit('data', reply);
+
+  this.logger.info('"Webhook" trigger is done');
+}
+
+module.exports.process = processTrigger;
