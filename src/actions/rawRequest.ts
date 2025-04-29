3e5078402bb1b91499a13369276ab9438133270f
if0s <if0s.3d@gmail.com>
2025-04-29 15:42:36 +0300

    dev

--- src/actions/rawRequest.ts
+++ src/actions/rawRequest.ts
@@ -1,23 +1,19 @@
 import { messages } from 'elasticio-node';
-import Client from '../client';
-import inSchema from '../schemas/actions/rawRequest.in.json';
-
-const allowedMethods = inSchema.properties.method.enum;
 
 export async function processAction(msg, cfg) {
   this.logger.info('"Make Raw Request" action started');
-  const client = new Client(this, cfg);
-  const { url, method, data } = msg.body;
-  if (!allowedMethods.includes(method)) {
-    throw new Error(`Method "${method}" is not supported! Supported methods are: ${JSON.stringify(allowedMethods)}`);
-  }
-  const response = await client.apiRequest({ url, method, data });
-  this.logger.info('request is done, emitting...');
-  return messages.newMessageWithBody({
-    statusCode: response.status,
-    headers: response.headers,
-    responseBody: response.data
-  });
+  const { configData } = cfg;
+  return messages.newMessageWithBody(msg.body);
 }
 
+export const getMetaModel = async function getMetaModel(cfg) {
+  const { configData } = cfg;
+  const inMeta = JSON.parse(configData);
+  return {
+    in: inMeta,
+    our: {}
+  };
+};
+
 module.exports.process = processAction;
+module.exports.getMetaModel = getMetaModel;
